f9f0af5c4d61dc0860c99e20ff219afa0d315c55
suribabu <suri.babu988@gmail.com>
2020-07-01 12:54:16 +0530

    ui changed

--- js/custom.js
+++ js/custom.js
@@ -100,34 +100,6 @@
 		}
 	});
 
-	// $(document).ready(function () {
-	// 	var owl = $('.owl-carousel');
-	// 	owl.owlCarousel({
-	// 		loop: true,
-	// 		autoplay: true,
-	// 		autoplayTimeout: 3000,
-	// 		autoplayHoverPause: true,
-	// 		margin: 10,
-	// 			responsiveClass: true,
-	// 				responsive: {
-	// 		0: {
-	// 			items: 1,
-	// 				nav: true
-	// 		},
-	// 		600: {
-	// 			items: 3,
-	// 				nav: false
-	// 		},
-	// 		1000: {
-	// 			items: 5,
-	// 				nav: true,
-	// 			loop: true
-	// 		}
-	// 	}
-	// 	});
-
-	// });
-
 	/* ==============================================
 	   CONTACT FORM
 	=============================================== */
